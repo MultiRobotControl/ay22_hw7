241cd92165c909cda479d7b3b542a6bccde7a10a
Mccarrin <developer@ip-172-30-58-148.ec2.internal>
2021-11-19 14:37:22 -0800

    Forgot to add USV odom callback

---
+++ matlab/usv_odom_callback.m
@@ -0,0 +1,10 @@
+function usv_odom_callback(~, msg)
+
+% Example callback function to be called with odometry message
+
+% For testing only - print a message when this function is called.
+% disp('Received USV Odometry')
+
+% Declare global variables and store Odometry message
+global USV_ODOM;
+USV_ODOM = msg;
